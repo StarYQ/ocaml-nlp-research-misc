c9a6888d169d84904e4f1a53a1c0c38345d819bd
Arnab Bhowmik <46697160+StarYQ@users.noreply.github.com>
2025-01-09 11:20:04 -0500

    Removed inputs from being built inside the record so they can be passed in during runtime

--- perceptron/bin/main.ml
+++ perceptron/bin/main.ml
@@ -6,14 +6,14 @@ let step x = if x>=0.0 then 1.0 else 0.0;;
 
 let inputs = [|1.0; 3.0|];;
 let weights = [|2.0; 4.5|];;
-let p1 = Perceptron.init inputs weights ~-.14.45 sigmoid;;
-let p2 = Perceptron.init inputs weights 0.63 relu;;
-let p3 = Perceptron.init inputs weights ~-.0.32 step;;
+let p1 = Perceptron.create weights ~-.14.45 sigmoid;;
+let p2 = Perceptron.create weights 0.63 relu;;
+let p3 = Perceptron.create weights ~-.0.32 step;;
 
 
 let () =
-  let res1 = Perceptron.output p1 in
-  let res2 = Perceptron.output p2 in 
-  let res3 = Perceptron.output p3 in
+  let res1 = Perceptron.output p1 inputs in
+  let res2 = Perceptron.output p2 inputs in 
+  let res3 = Perceptron.output p3 inputs in
   Printf.printf "Sigmoid: %f\n ReLU: %f\n Heaviside Step: %f\n" 
   res1 res2 res3;;
--- perceptron/lib/perceptron.ml
+++ perceptron/lib/perceptron.ml
@@ -1,23 +1,21 @@
 module Perceptron = struct 
   type p = {
-    inputs: float array;
     weights: float array;
     bias: float;
     activation: float -> float;
   }
 
-  let init inputs weights bias activation = {
-    inputs; 
+  let create weights bias activation = {
     weights;
     bias; 
     activation
   }
 
-  let output p = 
+  let output p inputs = 
     let weighted_sum = 
       let sum = ref 0.0 in 
-      for i=0 to (Array.length p.inputs)-1 do 
-        sum := !sum +. (p.inputs.(i) *. p.weights.(i)) 
+      for i=0 to (Array.length inputs)-1 do 
+        sum := !sum +. (inputs.(i) *. p.weights.(i)) 
       done;
       !sum in p.activation (weighted_sum +. p.bias)
 end;;
